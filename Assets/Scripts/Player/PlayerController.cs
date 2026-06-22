8b07ed685e175745e7b3a3815f44edbccae1ef57
hieunt26 <hieunt26@vng.com.vn>
2026-06-22 12:57:09 +0700

    wip attack anim

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,31 +8,41 @@ public class PlayerController : MonoBehaviour
     public InputActionAsset InputActions;
     public Rigidbody2D Rb;
 
-    [Header("Stats")]
+    [Header("===== Stats =====")]
+
     // Walk
-    [SerializeField] private float walkSpeed;
-    [Min(0.2f)][SerializeField] private float speedBuff; // divided by SpeedBuff everywhere -> ensure not too small
-    public float SpeedBuff => speedBuff;
+    [Header("Walk")]
+    [SerializeField] private float m_walkSpeed;
+    [Min(0.2f)][SerializeField] private float m_speedBuff; // divided by SpeedBuff everywhere -> ensure not too small
+    public float SpeedBuff => m_speedBuff;
     public Action<float> OnSpeedBuffChange;
+
     // Dash
-    [SerializeField] private float dashSpeed;
+    [Header("Dash")]
+    [SerializeField] private float m_dashSpeed;
     public GhostTrailController GhostTrailCtrl;
     public float DashCooldown;
-    private float dashTimer;
+    private float m_dashTimer;
     public float DashTime;
     public bool IsDashing;
     public Vector2 DashDir;
 
-    [Header("Hand")]
-    public WeaponController Weapon;
+    // Attack
+    [Header("Attack")]
     public Vector2 FacingDir = new(0, -1);
     public Animator SlashAnim;
     public bool IsAttacking;
+    public float AttackCooldown;
+    private float m_attackTimer;
+
+    [Header("Hand")]
+    public WeaponController Weapon;
 
     // Inputs
-    private InputAction m_moveAction;
-    private InputAction m_dashAction;
-    private InputAction m_attackAction;
+    [Header("Inputs")]
+    [SerializeField] private InputAction m_moveAction;
+    [SerializeField] private InputAction m_dashAction;
+    [SerializeField] private InputAction m_attackAction;
     public Vector2 MoveVal;
     public bool PressDashThisFrame;
     public bool AttackThisFrame;
@@ -56,10 +66,11 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (dashTimer >= 0) dashTimer -= Time.deltaTime;
+        if (m_dashTimer >= 0) m_dashTimer -= Time.deltaTime;
+        if (m_attackTimer >= 0) m_attackTimer -= Time.deltaTime;
         MoveVal = m_moveAction.ReadValue<Vector2>();
-        PressDashThisFrame = dashTimer < 0f && m_dashAction.IsPressed();
-        AttackThisFrame = !IsAttacking && m_attackAction.IsPressed();
+        PressDashThisFrame = m_dashTimer < 0f && m_dashAction.IsPressed();
+        AttackThisFrame = m_attackTimer < 0f && m_attackAction.IsPressed();
     }
 
     void FixedUpdate()
@@ -75,18 +86,23 @@ public class PlayerController : MonoBehaviour
 
     public float GetSpeed()
     {
-        var baseSpeed = IsDashing ? dashSpeed : walkSpeed;
+        var baseSpeed = IsDashing ? m_dashSpeed : m_walkSpeed;
         return baseSpeed * SpeedBuff;
     }
 
     public void SetSpeedBuff(float newVal)
     {
-        speedBuff = newVal;
+        m_speedBuff = newVal;
         OnSpeedBuffChange?.Invoke(SpeedBuff);
     }
 
     public void ResetDashTimer()
     {
-        dashTimer = DashCooldown;
+        m_dashTimer = DashCooldown;
+    }
+
+    public void ResetAttackTimer()
+    {
+        m_attackTimer = AttackCooldown;
     }
 }
--- Assets/Scripts/Weapon/SlashController.cs
+++ Assets/Scripts/Weapon/SlashController.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class SlashController : MonoBehaviour
 {
     public PlayerController Player;
+    public float Radius;
 
     public void StartAttack()
     {
         Player.IsAttacking = true;
+
+        Player.Weapon.gameObject.SetActive(false);
+        transform.localEulerAngles = new(0, 0, Mathf.Atan2(Player.FacingDir.y, Player.FacingDir.x) * Mathf.Rad2Deg);
+        transform.localPosition = Player.FacingDir * Radius;
     }
 
     public void EndAttack()
     {
         Player.IsAttacking = false;
+        Player.ResetAttackTimer();
+        Player.Weapon.gameObject.SetActive(true);
     }
 }
